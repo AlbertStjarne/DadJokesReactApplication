84ce771e6ea00685f03e0cfdcec13695bddc6a8b
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-24 16:23:27 +0200

    using Set to prevent duplicate jokes, adds error handling

--- src/JokeList.js
+++ src/JokeList.js
@@ -6,7 +6,7 @@ import './JokeList.css';
 
 class JokeList extends Component {
   static defaultProps = {
-    numJokesToGet: 10,
+    numJokesToGet: 3,
   };
 
   constructor(props) {
@@ -15,6 +15,8 @@ class JokeList extends Component {
       jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
       loading: false,
     };
+    this.seenJokes = new Set(this.state.jokes.map(j => j.text));
+    console.log(this.seenJokes);
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -25,21 +27,32 @@ class JokeList extends Component {
   }
 
   async getJokes() {
-    let jokes = [];
-    while (jokes.length < this.props.numJokesToGet) {
-      let res = await axios.get('https://icanhazdadjoke.com/', {
-        headers: { Accept: 'application/json' },
-      });
-      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
+    try {
+      let jokes = [];
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get('https://icanhazdadjoke.com/', {
+          headers: { Accept: 'application/json' },
+        });
+        let newJoke = res.data.joke;
+        if (!this.seenJokes.has(newJoke)) {
+          jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
+        } else {
+          console.log('found a duplicate');
+          console.log(newJoke);
+        }
+      }
+      this.setState(
+        st => ({
+          loading: false,
+          jokes: [...st.jokes, ...jokes],
+        }),
+        () =>
+          window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+      );
+    } catch (err) {
+      alert(err);
+      this.setState({ loading: false });
     }
-    this.setState(
-      st => ({
-        loading: false,
-        jokes: [...st.jokes, ...jokes],
-      }),
-      () =>
-        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
-    );
   }
 
   handleVote(id, delta) {
